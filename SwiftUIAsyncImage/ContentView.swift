d33caba3589ab1ac92898808c0b59a05220d8451
fatih bektas <fatih.bektass@gmail.com>
2022-06-22 12:47:03 +0300

    Add image extension

--- SwiftUIAsyncImage/ContentView.swift
+++ SwiftUIAsyncImage/ContentView.swift
@@ -7,11 +7,41 @@
 
 import SwiftUI
 
+extension Image {
+    func imageModifier() -> some View{
+        self
+            .resizable()
+            .scaledToFit()
+    }
+    
+    func iconModifier() -> some View{
+        self
+            .imageModifier()
+            .frame(maxWidth: 128)
+            .foregroundColor(.purple)
+            .opacity(0.5)
+    }
+}
+
+
 struct ContentView: View {
     private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // MARK: - 1. BASIC
+        //AsyncImage(url: URL(string: imageURL))
+        
+        // MARK: - 2. SCALE
+        //AsyncImage(url: URL(string: imageURL), scale: 2.0)
+        
+        // MARK: - 3. PLACEHOLDER
+        AsyncImage(url:URL(string: imageURL))  {image in image.imageModifier()
+            
+        } placeholder: {
+            Image(systemName: "photo.circle.fill").iconModifier()
+                
+        }
+        .padding(40)
     }
 }
 
